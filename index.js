c40e88fbb9e229996eb3ac6dbb8696b94cf5d3ee
Tad Martin <tadfmartin@gmail.com>
2023-03-20 16:10:19 -0400

    updated logo div font size updated validation methods, passCheck function refactored html set up to only have one form

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 function passCheck() {
     var x = document.getElementById("password");
     var y = document.getElementById("password_con");
+    var errormsg = document.getElementById("errormsg");
     if (x.value === y.value) {
-        x.style.border = "1px solid blue";
-        y.style.border = "1px solid blue";
-        console.log(x);
+        // x.style.border = "1px solid blue";
+        // y.style.border = "1px solid blue";
+        y.setCustomValidity("");
+        x.setCustomValidity("");
+        errormsg.style.color = "rgba(255, 0, 0, 0)";
     } else {
         // display error message
-        x.style.border = "1px solid red";
-        y.style.border = "1px solid red";
+        // x.style.border = "1px solid red";
+        // y.style.border = "1px solid red";
+        // alert("Passwords do not match");
+        y.setCustomValidity("Passwords do not match");
+        x.setCustomValidity("Passwords do not match");
+        errormsg.style.color = "rgba(255, 0, 0, 1)";
+
     }
 }
-
-console.log('hello')
-// console.log(passwd)
